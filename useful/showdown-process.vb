598ac0cb5e5d3f22cf10612ea6e474ee2d17ea08
Daniel Tixier <dgtixier@gmail.com>
2015-05-13 22:36:51 -0700

    Handles +20 values
    Eats up the +20 values of the 2002-2005 cards.

--- useful/showdown-process.vb
+++ useful/showdown-process.vb
@@ -5,14 +5,30 @@ Sub RangeToValue()
     Dim rngCell As Range
     Dim allMatches As Object
     Dim strPattern As String: strPattern = "[0-9]*"
+    Dim rangeOverTwentyPattern As String: rangeOverTwentyPattern = "-2[1-9]+"
     Dim regEx As New RegExp
+    Dim rangeOverTwenty As New RegExp
     Dim i As Object
     Dim j As Object
     Dim ii As Integer
     Dim jj As Integer
    
+    ' Set up regEx objects
+    With regEx
+        .Global = True
+        .MultiLine = False
+        .IgnoreCase = False
+        .Pattern = strPattern
+    End With
+    With rangeOverTwenty
+        .Global = True
+        .MultiLine = False
+        .IgnoreCase = False
+        .Pattern = rangeOverTwentyPattern
+    End With
+    
     ' ***BE SURE TO SET THIS***
-    Set rngCell = Range("G2:O2442")
+    Set rngCell = Range("G2:P1454")
 
     For Each cell In rngCell.Cells
         ' 1) Hyphen only
@@ -23,34 +39,26 @@ Sub RangeToValue()
             cell.Value = 1
         ' 3) Anything else with a hyphen
         ElseIf InStr(cell.Value, "-") Then
-            ' Set up regEx object
-            With regEx
-                .Global = True
-                .MultiLine = False
-                .IgnoreCase = False
-                .Pattern = strPattern
-            End With
             Set allMatches = regEx.Execute(cell.Value)
             If allMatches.Count = 3 Then ' Only one number then hyphen
                 cell.Value = 1
-            Else ' number before and after hyphen, do the math
+            Else ' number before and after hyphen
                 Set i = allMatches.Item(0)
                 Set j = allMatches.Item(2)
                 ii = CInt(i)
                 jj = CInt(j)
-                computedValue = jj - ii + 1
-                ' Debug.Print ii, jj, computedValue
+                ' Splitting into ifs because of the 2002-2005 over 20 vals
+                If jj > 20 And ii > 20 Then ' keep the first val only
+                    computedValue = ii
+                ElseIf jj > 20 Then ' throw away the vals over 20
+                    computedValue = 20 - ii + 1
+                Else ' do the math...
+                    computedValue = jj - ii + 1
+                End If
                 cell.Value = computedValue
-                ' Debug.Print cell.Address, cell.Value
             End If
         ' 4) Has a plus
         ElseIf InStr(cell.Value, "+") Then
-            With regEx
-                .Global = True
-                .MultiLine = False
-                .IgnoreCase = False
-                .Pattern = strPattern
-            End With
             Set allMatches = regEx.Execute(cell.Value)
             ' First get rid of the plus
             cell.Value = allMatches.Item(0)

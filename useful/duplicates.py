a0d10bcfd7b755aed904e785ead806841db6f7ae
Daniel Tixier <dgtixier@gmail.com>
2015-05-30 00:18:51 -0700

    All wotc pitchers

--- useful/duplicates.py
+++ useful/duplicates.py
@@ -1,6 +1,6 @@
 lines_seen = set() # holds lines already seen
 outfile = open("zzzzzz.sql", "w")
-for line in open("cards-tables.sql", "r"):
+for line in open("PitchersCopy.csv", "r"):
     if line not in lines_seen: # not a duplicate
         outfile.write(line)
         lines_seen.add(line)
